a8fefa71d06c42ad100b2a932324a0568e1eb6c9
Nathan Waite <natew821@gmail.com>
2021-02-12 09:10:35 -0700

    Animation is Correct

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,51 +52,56 @@ export default function Home() {
               animate={{ opacity: 1, translateY: 0 }}
               transition={{ delay: 9.5, duration: 0.5 }}
             >
+              {/* <a href="https://github.com/natew8"> */}
               <img
                 id={homeStyles.github}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/github.svg"
                 alt="github"
               />
+              {/* </a>
+              <a href="https://www.linkedin.com/in/natew8/"> */}
               <img
                 id={homeStyles.linked}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/linkedin.svg"
                 alt="linkedIn"
               />
+              {/* </a>
+              <a href="https://www.instagram.com/natew8/"> */}
               <img
                 id={homeStyles.ig}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/instagram.svg"
                 alt="IG"
               />
+              {/* </a>
+              <a href="https://open.spotify.com/artist/57MyPA2CqgcigML6JnIixn?si=DiuUjiZtRwq8PWTBPQGCHQ"> */}
               <img
                 id={homeStyles.spotify}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/spotify.svg"
                 alt="Spotify"
               />
+              {/* </a>
+              <a href="https://music.apple.com/us/artist/nate-waite/1471148574"> */}
               <img
                 id={homeStyles.apple}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/applemusic.svg"
                 alt="AppleMusic"
               />
+              {/* </a>
+              <a href="https://www.youtube.com/channel/UCq3wQFwdBIa_xCFHQvS8G2A/featured"> */}
               <img
                 id={homeStyles.youtube}
                 className={homeStyles.icon}
                 src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/youtube.svg"
                 alt="youtube"
               />
+              {/* </a> */}
             </motion.span>
           </div>
-          {/* <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 9.5, duration: 0.5 }}
-            src="https://nw-portfolio-images.s3-us-west-1.amazonaws.com/natewaite.jpg"
-            alt="nate"
-          /> */}
           <Flower />
         </span>
       </div>
